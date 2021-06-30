855e132c6cf463e9755e04b290d0c2ffd34f56b1
Kyunghwan Kim <khsyee@gmail.com>
2021-06-30 18:38:52 +0900

    First commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 if ! which brew
 then
     printf '\n Homebrew not installed in your MacOS! \n'
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # sudo authority
@@ -16,25 +16,35 @@ cd brew
 # BrewFile execution command
 brew bundle
 
-# When downloading with cask, it is the same as the one downloaded from the web.
-# so when executed, it is not executed because of a security problem.
-# A command to remove the quarantine property for the downloaded file to immediately run the application installed with cask
-sudo xattr -dr com.apple.quarantine /Applications/iTerm.app
-sudo xattr -dr com.apple.quarantine /Applications/Docker.app
-sudo xattr -dr com.apple.quarantine /Applications/MySQLWorkbench.app
-sudo xattr -dr com.apple.quarantine /Applications/Google\ Chrome.app
-sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code.app
-sudo xattr -dr com.apple.quarantine /Applications/Postman.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Word.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Excel.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ PowerPoint.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Outlook.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ OneNote.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Teams.app
-sudo xattr -dr com.apple.quarantine /Applications/Wireshark.app
-sudo xattr -dr com.apple.quarantine /Applications/Karabiner-elements.app
-sudo xattr -dr com.apple.quarantine /Applications/Sublime\ Text.app
-sudo xattr -dr com.apple.quarantine /Applications/Notion.app
+# Additional command
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Remove user name on prompt
+echo "\n# DEFAULT_USER" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "DEFAULT_USER='$(whoami)'" >> ${ZDOTDIR:-$HOME}/.zshrc
+# zsh plugin
+echo "\n# zsh plugin" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+# fzf
+echo "\n# fzf" >> ${ZDOTDIR:-$HOME}/.zshrc
+$(brew --prefix)/opt/fzf/install --all
+
+# # When downloading with cask, it is the same as the one downloaded from the web.
+# # so when executed, it is not executed because of a security problem.
+# # A command to remove the quarantine property for the downloaded file to immediately run the application installed with cask
+# sudo xattr -dr com.apple.quarantine /Applications/iTerm.app
+# sudo xattr -dr com.apple.quarantine /Applications/Docker.app
+# sudo xattr -dr com.apple.quarantine /Applications/Google\ Chrome.app
+# sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Word.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Excel.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ PowerPoint.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Outlook.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ OneNote.app
+# sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Teams.app
+# sudo xattr -dr com.apple.quarantine /Applications/Karabiner-elements.app
+# sudo xattr -dr com.apple.quarantine /Applications/Notion.app
 
 # installation complete message
 printf '\n installation complete! \n'
